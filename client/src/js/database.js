33631e92bb749e8cd9b4a36580503237e19702d5
Rhoward1 <ryanmhoward@gmail.com>
2022-05-18 15:29:43 -0500

    fixed install.js and worked on database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,11 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-      
-  
+export const putDb = async (content) => {
+  console.log('PUT to jateDB');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, value: content });
+  const result = await request;
+  console.log('data saved to the jateDB', result.value);
+}
+  // console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
 
-initdb();
+
+  // TODO: Add logic for a method that gets all the content from the database
+  export const getDb = async () => {
+    console.log('GET all from the database');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('GET data from database', result);
+  }
+    // console.error('getDb not implemented');
+
+    initdb();
